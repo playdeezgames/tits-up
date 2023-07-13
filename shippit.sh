1e6f3980157c38794f39607548892fc5262e79e8
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-07-13 05:29:38 -0500

    shipped it!

--- shippit.sh
+++ shippit.sh
@@ -1,9 +1,9 @@
 rm -rf ./pub-linux
 rm -rf ./pub-windows
 rm -rf ./pub-mac
-dotnet publish ./src/TitsUp/TitsUp.vbproj -o ./pub-linux -c Release --sc -r linux-x64
-dotnet publish ./src/TitsUp/TitsUp.vbproj -o ./pub-windows -c Release --sc -r win-x64
-dotnet publish ./src/TitsUp/TitsUp.vbproj -o ./pub-mac -c Release --sc -r osx-x64
+dotnet publish ./src/TitsUp/TitsUUp.vbproj -o ./pub-linux -c Release --sc -r linux-x64
+dotnet publish ./src/TitsUp/TitsUUp.vbproj -o ./pub-windows -c Release --sc -r win-x64
+dotnet publish ./src/TitsUp/TitsUUp.vbproj -o ./pub-mac -c Release --sc -r osx-x64
 butler push pub-windows thegrumpygamedev/tits-uup:windows
 butler push pub-linux thegrumpygamedev/tits-uup:linux
 butler push pub-mac thegrumpygamedev/tits-uup:mac
