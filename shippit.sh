d18884c18630799ffe2f47f7abd5c68452f5e6b3
Ernest Pazera <ernestpazera@gmail.com>
2025-01-16 12:49:00 -0600

    update to dotnet 8

--- shippit.sh
+++ shippit.sh
@@ -4,8 +4,8 @@ rm -rf ./pub-mac
 dotnet publish ./src/TitsUp/TitsUUp.vbproj -o ./pub-linux -c Release --sc -r linux-x64
 dotnet publish ./src/TitsUp/TitsUUp.vbproj -o ./pub-windows -c Release --sc -r win-x64
 dotnet publish ./src/TitsUp/TitsUUp.vbproj -o ./pub-mac -c Release --sc -r osx-x64
-butler push pub-windows thegrumpygamedev/tits-uup:windows
-butler push pub-linux thegrumpygamedev/tits-uup:linux
-butler push pub-mac thegrumpygamedev/tits-uup:mac
-git add -A
-git commit -m "shipped it!"
+#butler push pub-windows thegrumpygamedev/tits-uup:windows
+#butler push pub-linux thegrumpygamedev/tits-uup:linux
+#butler push pub-mac thegrumpygamedev/tits-uup:mac
+#git add -A
+#git commit -m "shipped it!"
